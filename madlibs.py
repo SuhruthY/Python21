e734f9f31d6c9a6444761682f31276345ca6d6f1
3D-soul <suhruthyambakam822001@gmail.com>
2020-12-21 18:19:06 +0530

    removed files temporarily

--- madlibs.py
+++
@@ -1,5 +0,0 @@
-noun_01 = input("Enter a animal:")
-noun_02 = input("Where is th animal living in:")
-adj = input("Enter an adjective:")
-
-print("A " + noun_01 + " is in his/her " + noun_02 + " for a " + adj + " family night.")
